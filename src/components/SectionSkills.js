85a20dc6d904b47715744f11a180f4837d8346d4
Bueribrian <bueribrian@hotmail.com>
2019-06-18 13:48:27 -0300

    first commit

--- src/components/SectionSkills.js
+++ src/components/SectionSkills.js
@@ -3,31 +3,6 @@ import Title from './Title'
 
 export default () => {
   return (
-    <section id='sectionSkills'>
-      <div className='container-big'>
-        <Title text='Habilidades' />
-        <div className='skill-container'>
-          <div className='skill'>
-            <img alt='skill icon' src='./img/gear.svg' className='skill-img' ></img>
-            <p>Esto es un texto de relleno para hacer espacion y poder maquetar el texto y visualizar correctamente el espacion en la seccion por lo tanto no tiene ningun significa ni valor en esta pagina </p>
-          </div>
-
-          <div className='skill'>
-            <img alt='skill icon' src='./img/heart.svg' className='skill-img' ></img>
-            <p>Esto es un texto de relleno para hacer espacion y poder maquetar el texto y visualizar correctamente el espacion en la seccion por lo tanto no tiene ningun significa ni valor en esta pagina </p>
-          </div>
-
-          <div className='skill'>
-            <img alt='skill icon' src='./img/phone.svg' className='skill-img' ></img>
-            <p>Esto es un texto de relleno para hacer espacion y poder maquetar el texto y visualizar correctamente el espacion en la seccion por lo tanto no tiene ningun significa ni valor en esta pagina </p>
-          </div>
-
-          <div className='skill'>
-            <img alt='skill icon' src='./img/head.svg' className='skill-img' ></img>
-            <p>Esto es un texto de relleno para hacer espacion y poder maquetar el texto y visualizar correctamente el espacion en la seccion por lo tanto no tiene ningun significa ni valor en esta pagina </p>
-          </div>
-        </div>
-      </div>
-    </section>
+    <></>
   )
 }
