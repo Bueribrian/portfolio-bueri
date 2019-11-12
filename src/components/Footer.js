9702166826686adc2357632ca233f361a2e03aaf
Brian Bueri <32884323+Bueribrian@users.noreply.github.com>
2019-11-12 00:24:17 -0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,10 @@ export default () => {
     <footer id='footer'>
       <a className="btn btn-purple" href='#email'>Contactar (msg);</a>
       <div className='social-media-footer'>
-        <a target='_blank' rel="noopener noreferrer" href='https://www.linkedin.com/in/brian-bueri-838845155/'>
+        <a target='_blank' rel="noopener noreferrer" href='https://www.linkedin.com/'>
         <img alt='figure footer social' style={{'height':'10rem','margin':'2rem'}} src='./img/github.svg'></img>
         </a>
-        <a target='_blank' rel="noopener noreferrer" href='https://github.com/Bueribrian/'>
+        <a target='_blank' rel="noopener noreferrer" href='https://github.com/'>
         <img alt='figure footer social' style={{'height':'12.5rem','margin':'1rem'}} src='./img/linkedin.svg'></img>
         </a>
       </div>
